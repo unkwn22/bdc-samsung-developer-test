c68dda02312f086cb31cd8daaa96eb556e1adb63
Jason <rimmybear2255@gmail.com>
2024-12-27 14:24:58 +0900

    feat: initial commit

---
+++ src/main/java/com/example/bdcsamsungdevelopertest/BdcSamsungDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bdcsamsungdevelopertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdcSamsungDeveloperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdcSamsungDeveloperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bdcsamsungdevelopertest/BdcSamsungDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.example.bdcsamsungdevelopertest;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BdcSamsungDeveloperTestApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
