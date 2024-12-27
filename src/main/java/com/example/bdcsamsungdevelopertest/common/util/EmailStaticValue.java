0b811d2a54234e68831f65a61efaabadcdb0706e
Jason <rimmybear2255@gmail.com>
2024-12-28 01:24:35 +0900

    feat: email static value utility
    - created parse extension samsung email static value

--- src/main/java/com/example/bdcsamsungdevelopertest/common/util/EmailStaticValue.java
+++ src/main/java/com/example/bdcsamsungdevelopertest/common/util/EmailStaticValue.java
@@ -3,4 +3,5 @@ package com.example.bdcsamsungdevelopertest.common.util;
 public class EmailStaticValue {
 
     public static final String EMAIL_PATTERN_REGEX = "^[a-zA-Z0-9_.+-]+@samsung.com";
+    public static final String SAMSUNG_EMAIL = "@samsung.com";
 }
